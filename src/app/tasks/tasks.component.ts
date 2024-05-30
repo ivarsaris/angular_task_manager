b95a9215ead7315abe15c753729260efea0845c4
Ivar <ivar.saris@blueelevation.nl>
2024-05-30 07:39:24 +0200

    refactored hardcoded task to dynamic content

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,13 +1,17 @@
+import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
+import { tasks_list } from './tasks.list';
 
 @Component({
   selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [NgFor],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
+  tasks_list = tasks_list;
+
   expandDropdownMenu(id: string, event: Event) {
     const target = event.target as HTMLElement | null;
     if (target) {
---
+++ src/app/tasks/tasks.list.ts
@@ -0,0 +1,31 @@
+export const tasks_list = [
+    {
+        id: 1,
+        title: 'Implement low tension support Eindhoven',
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum quis
+        facilisis neque. Nunc mattis pretium nisi sed volutpat. Maecenas ac ante
+        sit amet nisi lacinia finibus ut et lorem. Quisque accumsan massa elit, et
+        accumsan augue tempus maximus. Pellentesque dolor ex, ornare nec eleifend
+        et, cursus ac tortor. Praesent luctus auctor lobortis. Cras hendrerit
+        augue tortor. Donec pellentesque hendrerit lectus, quis laoreet lectus
+        convallis eu.`,
+        status: 'to do',
+        date_created: '26.05.2024',
+        date_deadline: '26.06.2024',
+        priority: 4,
+        assignee_id: 1, 
+    },
+    {
+        id: 2,
+        title: 'Incident fire electricity dispenser',
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum quis
+        facilisis neque. Nunc mattis pretium nisi sed volutpat. Maecenas ac ante
+        sit amet nisi lacinia finibus ut et lorem. Quisque accumsan massa elit, et
+        accumsan augue tempus maximus.`,
+        status: 'to do',
+        date_created: '28.05.2024',
+        date_deadline: '30.05.2024',
+        priority: 1,
+        assignee_id: 1, 
+    }
+]
