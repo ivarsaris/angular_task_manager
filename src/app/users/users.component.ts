cf288b3eaec8e36f60c6e7e0b921e7e3aea3ee69
Ivar <ivar.saris@blueelevation.nl>
2024-05-30 07:38:46 +0200

    user component, added active state to user

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,14 +1,19 @@
-import { NgFor } from '@angular/common';
+import { NgClass, NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { users_list } from './users.list';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, NgIf, NgClass],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
 export class UsersComponent {
   users_list = users_list;
+  selected_user: number = 1;
+
+  selectUser(id: number) {
+    this.selected_user = id;
+  }
 }
